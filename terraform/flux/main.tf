54b9fe5b04259e0e9fc2bcb89790f3371b995ebe
Jax Gauthier <jax@justin-tech.com>
2023-06-06 20:08:01 -0400

    update terraform

--- terraform/flux/main.tf
+++ terraform/flux/main.tf
@@ -43,22 +43,6 @@ data "tfe_outputs" "kubeconfig" {
   workspace    = "kube-cluster"
 }
 
-provider "flux" {}
-
-provider "kubernetes" {
-  host                   = yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["clusters"][0]["cluster"]["server"]
-  cluster_ca_certificate = base64decode(yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["clusters"][0]["cluster"]["certificate-authority-data"])
-  client_certificate     = base64decode(yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["users"][0]["user"]["client-certificate-data"])
-  client_key             = base64decode(yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["users"][0]["user"]["client-key-data"])
-}
-
-provider "kubectl" {
-  host                   = yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["clusters"][0]["cluster"]["server"]
-  cluster_ca_certificate = base64decode(yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["clusters"][0]["cluster"]["certificate-authority-data"])
-  client_certificate     = base64decode(yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["users"][0]["user"]["client-certificate-data"])
-  client_key             = base64decode(yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["users"][0]["user"]["client-key-data"])
-}
-
 provider "github" {
   owner = data.sops_file.secrets.data["gh_owner"]
   token = data.sops_file.secrets.data["gh_token"]
@@ -70,140 +54,36 @@ variable "repository_name" {
   description = "github repository name"
 }
 
-variable "repository_visibility" {
-  type        = string
-  default     = "private"
-  description = "How visible is the github repo"
-}
-
-variable "branch" {
-  type        = string
-  default     = "main"
-  description = "branch name"
-}
-
-variable "target_path" {
-  type        = string
-  default     = "cluster/production"
-  description = "flux sync target path"
-}
-
-# SSH
-locals {
-  known_hosts = "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg="
-}
-
-resource "tls_private_key" "main" {
+resource "tls_private_key" "flux" {
   algorithm   = "ECDSA"
   ecdsa_curve = "P256"
 }
 
-# Flux
-data "flux_install" "main" {
-  target_path = var.target_path
+resource "github_repository_deploy_key" "this" {
+  title      = "Flux"
+  repository = var.repository_name
+  key        = tls_private_key.flux.public_key_openssh
+  read_only  = "false"
 }
 
-data "flux_sync" "main" {
-  target_path = var.target_path
-  url         = "ssh://git@github.com/${data.sops_file.secrets.data["gh_owner"]}/${var.repository_name}.git"
-  branch      = var.branch
-}
-
-# Kubernetes
-resource "kubernetes_namespace" "flux_system" {
-  metadata {
-    name = "flux-system"
+provider "flux" {
+  kubernetes = {
+    host                   = yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["clusters"][0]["cluster"]["server"]
+    cluster_ca_certificate = base64decode(yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["clusters"][0]["cluster"]["certificate-authority-data"])
+    client_certificate     = base64decode(yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["users"][0]["user"]["client-certificate-data"])
+    client_key             = base64decode(yamldecode(data.tfe_outputs.kubeconfig.values.kubeconfig)["users"][0]["user"]["client-key-data"])
   }
-
-  lifecycle {
-    ignore_changes = [
-      metadata[0].labels,
-    ]
-  }
-}
-
-data "kubectl_file_documents" "install" {
-  content = data.flux_install.main.content
-}
-
-data "kubectl_file_documents" "sync" {
-  content = data.flux_sync.main.content
-}
-
-locals {
-  install = [for v in data.kubectl_file_documents.install.documents : {
-    data : yamldecode(v)
-    content : v
+  git = {
+    url = "ssh://git@github.com/${data.sops_file.secrets.data["gh_owner"]}/${var.repository_name}.git"
+    ssh = {
+      username    = "git"
+      private_key = tls_private_key.flux.private_key_pem
     }
-  ]
-  sync = [for v in data.kubectl_file_documents.sync.documents : {
-    data : yamldecode(v)
-    content : v
-    }
-  ]
-}
-
-resource "kubectl_manifest" "install" {
-  for_each   = { for v in local.install : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux_system]
-  yaml_body  = each.value
-}
-
-resource "kubectl_manifest" "sync" {
-  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-  depends_on = [kubernetes_namespace.flux_system]
-  yaml_body  = each.value
-}
-
-resource "kubernetes_secret" "main" {
-  depends_on = [kubectl_manifest.install]
-
-  metadata {
-    name      = data.flux_sync.main.secret
-    namespace = data.flux_sync.main.namespace
-  }
-
-  data = {
-    identity       = tls_private_key.main.private_key_pem
-    "identity.pub" = tls_private_key.main.public_key_pem
-    known_hosts    = local.known_hosts
   }
 }
 
-# GitHub
-data "github_repository" "main" {
-  name = var.repository_name
-}
+resource "flux_bootstrap_git" "this" {
+  depends_on = [github_repository_deploy_key.this]
 
-data "github_branch" "main" {
-  repository = data.github_repository.main.name
-  branch     = var.branch
-}
-
-resource "github_repository_deploy_key" "main" {
-  title      = "flux-production"
-  repository = data.github_repository.main.name
-  key        = tls_private_key.main.public_key_openssh
-  read_only  = true
-}
-
-resource "github_repository_file" "install" {
-  repository = data.github_repository.main.name
-  file       = data.flux_install.main.path
-  content    = data.flux_install.main.content
-  branch     = var.branch
-}
-
-resource "github_repository_file" "sync" {
-  repository = data.github_repository.main.name
-  file       = data.flux_sync.main.path
-  content    = data.flux_sync.main.content
-  branch     = var.branch
-}
-
-resource "github_repository_file" "kustomize" {
-  repository = data.github_repository.main.name
-  file       = data.flux_sync.main.kustomize_path
-  content    = data.flux_sync.main.kustomize_content
-  branch     = var.branch
+  path = "clusters/production"
 }
